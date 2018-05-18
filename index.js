a98b56e03b4b6a41de12e2b4d972cbf1078300bf
Mário Valney <mario@vizir.com.br>
2018-05-18 18:27:29 -0300

    [Feat] Create command --version
    Added Liftoff
    Added Chalk to help log
    Added CLI interface

--- index.js
+++ index.js
@@ -1,7 +1,34 @@
 'use strict';
 
+var interpret = require('interpret');
+var liftoff = require('liftoff');
+var minimist = require('minimist');
+
+var commander = require('./lib/commander');
+var title = require('./lib/cli/title');
+
+const Liftoff = new liftoff({
+  name: 'wpvizir',
+  processTitle: title('wpvizir', process.argv.slice(2)),
+  configName: 'vizirfile',
+  extensions: interpret.jsVariants
+});
+
+var options = minimist(process.argv.slice(2));
+
+function invoke(env) {
+  if (options.version || options.version) {
+    commander('version');
+    process.exit(0);
+  }
+}
+
 function run() {
-    return console.log( 'Hello World' );
+  Liftoff.launch({
+    cwd: options.cwd,
+    configPath: options.vizirfile,
+    completion: options.completion,
+  }, invoke);
 }
 
 module.exports = run;
