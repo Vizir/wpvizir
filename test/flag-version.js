328565f0d8df5bce395ee59b5ac88a0f56410c88
Mário Valney <mario@vizir.com.br>
2018-05-18 21:11:44 -0300

    [Test] Added test to -v

--- test/flag-version.js
+++ test/flag-version.js
@@ -7,8 +7,18 @@ var version = require('../package.json').version;
 
 describe('flag: --version', function() {
 
-  it('print the CLI version', function(done) {
+  it('print the CLI version using --version', function(done) {
     runner().command('--version').run(function(err, stdout, stderr) {
+      expect(err).toEqual(null);
+      expect(stderr).toEqual('');
+      expect(stdout).toEqual('Version: ' + version);
+
+      done(err);
+    });
+  });
+
+  it('print the CLI version using -v', function(done) {
+    runner().command('-v').run(function(err, stdout, stderr) {
       expect(err).toEqual(null);
       expect(stderr).toEqual('');
       expect(stdout).toEqual('Version: ' + version);
