ec86af6d29e9b723565895ffdcc879749bb9f0a4
Mário Valney <mario@vizir.com.br>
2018-05-20 00:39:09 -0300

    [Test] For Did you say

---
+++ test/lib/cli/didyousay.js
@@ -0,0 +1,32 @@
+'use strict';
+
+var didyousay = require('../../../lib/cli/didyousay');
+var __ = require('../../../lib/translater');
+
+var expect = require('expect');
+var runner = require('../../helpers/runner');
+
+describe('lib: cli/didyousay', function() {
+
+  it('should return false if there\'s no suggestion', function(done) {
+    expect(didyousay('ofcoursethereisnosuggestionforthat')).toBe(false);
+    done();
+  });
+
+  it('should log suggestion', function(done) {
+    runner().command('version').run(function(err, stdout, stderr) {
+      expect(err).toEqual(null);
+      expect(stderr).toEqual('');
+      expect(stdout).toEqual(__('Did want to say that?') + "\n" + '    wpvizir --version');
+
+      done(err);
+    });
+  });
+
+  it('should return suggestion for: --version', function(done) {
+    expect(didyousay('version')).toBeTruthy();
+    expect(didyousay('v')).toBeTruthy();
+    done();
+  });
+
+});
