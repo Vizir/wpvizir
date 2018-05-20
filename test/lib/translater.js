11cc3b433f8eb02edb066091ed5e8ca6d7cce028
Mário Valney <mario@vizir.com.br>
2018-05-20 15:07:40 -0300

    [Feat] Locale as config

--- test/lib/translater.js
+++ test/lib/translater.js
@@ -2,21 +2,37 @@
 
 var expect = require('expect');
 var translater = require('../../lib/translater');
+var configurator = require('../../lib/configurator');
 
 describe('translater', function() {
 
   it('should translate the string', function(done) {
+    var locale = configurator.get('locale');
+
+    configurator.set('locale', 'pt_BR');
     expect(translater('Version: %s')).toEqual('Versão: %s');
+    configurator.set('locale', locale);
+
     done(0);
   });
 
   it('should translate the string with values as string', function(done) {
+    var locale = configurator.get('locale');
+
+    configurator.set('locale', 'pt_BR');
     expect(translater('Version: %s', '0.0.0')).toEqual('Versão: 0.0.0');
+    configurator.set('locale', locale);
+
     done(0);
   });
 
   it('should translate the string with values as object', function(done) {
+    var locale = configurator.get('locale');
+
+    configurator.set('locale', 'pt_BR');
     expect(translater('Version: {version}', {version: '0.0.0'})).toEqual('Versão: 0.0.0');
+    configurator.set('locale', locale);
+
     done(0);
   });
 
