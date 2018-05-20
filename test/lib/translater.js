4c0a9319d14ed9c2c1f29d6d492a614b2ea3eaf2
Mário Valney <mario@vizir.com.br>
2018-05-20 16:30:41 -0300

    [Feat] Translater cache locales to allow change in running time

--- test/lib/translater.js
+++ test/lib/translater.js
@@ -36,6 +36,17 @@ describe('translater', function() {
     done(0);
   });
 
+  it('should not translate if locale is invalid', function(done) {
+    var locale = configurator.get('locale');
+
+    configurator.set('locale', 'ofcourseanotvalidlocale');
+    expect(translater('Version: %s', '0.0.0')).toEqual('Version: 0.0.0');
+    configurator.set('locale', locale);
+
+    done(0);
+  });
+
+
   it('should return the string with no translations are found', function(done) {
     expect(translater('Of course there\'s no translation for that')).toEqual('Of course there\'s no translation for that');
     done(0);
